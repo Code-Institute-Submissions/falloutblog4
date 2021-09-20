b66cb8f0876ad1803727a51306828f5c3f5205d0
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-20 17:43:27 +0000

    Add category list to navbar across all pages excluding category page itself

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -10,10 +10,22 @@ class Home(ListView):
     template_name = 'home.html'
     ordering = ['-created_on']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(Home, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 class ArticleDetail(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetail, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 
 class AddPost(CreateView):
@@ -21,11 +33,23 @@ class AddPost(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(AddPost, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 class UpdatePost(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(UpdatePost, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 
 class DeletePost(DeleteView):
@@ -33,7 +57,14 @@ class DeletePost(DeleteView):
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(DeletePost, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 def CategoryView(request, cat):
     category_posts = Post.objects.filter(category=cat)
     return render(request, 'categories.html', {'cat': cat, 'category_posts': category_posts})
+
