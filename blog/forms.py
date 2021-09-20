6a01bb4df9743d9f8786dedf1ba685bd2e5d4205
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-20 23:27:37 +0000

    Fix error on posting new posts. Add style to register form. Fix like/unlike button

--- blog/forms.py
+++ blog/forms.py
@@ -17,8 +17,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title Here'}),
             'tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'authorname', 'type': 'hidden'}),
-            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Content Here'}),
         }
