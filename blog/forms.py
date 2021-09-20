94319d2476bd7ab0d79eeb324435d14bfc7ace70
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-20 18:31:52 +0000

    Add author to be automatic for user.

--- blog/forms.py
+++ blog/forms.py
@@ -17,7 +17,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title Here'}),
             'tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'authorname', 'type': 'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Content Here'}),
         }
