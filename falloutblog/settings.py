2bfa97e47ba663810321b128c9ede46298ead22b
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-21 09:31:12 +0000

    Add missing fcode to settings file

--- falloutblog/settings.py
+++ falloutblog/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
 ALLOWED_HOSTS = ['falloutblog4.herokuapp.com', 'localhost']
 
 
